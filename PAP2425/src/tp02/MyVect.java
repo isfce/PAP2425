6bf36296e1164ae46a3ae9b5e263fcbd30c852d5
VOZ <voisfce@gmail.com>
2024-12-04 21:55:12 +0100

    Exemples vecteurs

--- PAP2425/src/tp02/MyVect.java
+++ PAP2425/src/tp02/MyVect.java
@@ -1,6 +1,10 @@
 package tp02;
 
 public class MyVect {
+
+	public record RMinMax(int min, int max) {
+	};
+
 	/**
 	 * Affiche un vecteur d'entiers
 	 * v ne doit pas être null
@@ -56,19 +60,10 @@ public class MyVect {
 	}
 
 	/**
-	 * Retourne l'élément minimum du vecteur
+	 * Idem mais avec une boucle "pour"
 	 * @param v
 	 * @return
 	 */
-	public static int minElemV(int[] v) {
-		assert v.length > 0 : "Le vecteur ne peut pas être vide";
-		int maxi = v[0];
-		for (int elem : v)
-			if (maxi > elem)
-				maxi = elem;
-		return maxi;
-	}
-
 	public static int maxElemV2(int[] v) {
 		assert v.length > 0 : "Le vecteur ne peut pas être vide";
 		int maxi = v[0];
@@ -76,6 +71,20 @@ public class MyVect {
 			if (maxi < v[i])
 				maxi = v[i];
 		return maxi;
+	}
+
+	/**
+	 * Retourne l'élément minimum du vecteur
+	 * @param v
+	 * @return
+	 */
+	public static int minElemV(int[] v) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		int mini = v[0];
+		for (int elem : v)
+			if (mini > elem)
+				mini = elem;
+		return mini;
 	}
 
 	/**
@@ -95,21 +104,124 @@ public class MyVect {
 	 * @return
 	 */
 	public static int[] cloneV(int[] v) {
-		int[] cl = new int[v.length];
+		// création d'un nouveau vecteur
+		int[] clone = new int[v.length];
 		for (int i = 0; i < v.length; i++)
-			cl[i] = v[i];
-		return cl;
+			clone[i] = v[i];
+		return clone;
+	}
+
+	/**
+	 * Moyenne des éléments de V
+	 * ! taille de v >0
+	 * @param v
+	 * @return
+	 */
+	public static double avgElemV(int[] v) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		return (double) sommeV(v) / v.length;// force le calcul en réel
+	}
+
+	/**
+	 * Indique le nbr de fois qu'"elem" existe ds V
+	 * @param v
+	 * @param elem
+	 * @return
+	 */
+	public static int nbElemV(int[] v, int elem) {
+		int cpt = 0;
+		for (int e : v)
+			if (e == elem) // si objet ==> e.equals(elem)
+				cpt++;
+		return cpt;
+	}
+
+	/**
+	* Retourne le min et la max du vecteur
+	* @param v
+	* @return  [min,max]
+	*/
+	public static int[] minMaxElemV(int[] v) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		int[] res = { v[0], v[0] };
+		for (int i = 1; i < v.length; i++)
+			if (v[i] < res[0])
+				res[0] = v[i];
+			else if (v[i] > res[1])
+				res[1] = v[i];
+		return res;
+	}
+
+	/**
+	* Retourne le min et la max du vecteur
+	* @param v
+	* @return  Record[min,max]
+	*/
+	public static RMinMax minMaxElemV2(int[] v) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		int mini = v[0];
+		int maxi = v[0];
+		for (int i = 1; i < v.length; i++)
+			if (v[i] < mini)
+				mini = v[i];
+			else if (v[i] > maxi)
+				maxi = v[i];
+		return new RMinMax(mini, maxi);
+	}
+
+	/**
+	 * Vérifie si le texte est un palindrome
+	 * @param v un vecteur de char
+	 * @return
+	 */
+	public static boolean estPalindrome(char[] v) {
+		boolean pal = true;
+		int i = 0;
+		int j = v.length - 1;
+		while (pal && i < j) {
+			pal = v[i] == v[j];
+			i++;
+			j--;
+		}
+		return pal;
+	}
+
+	/**
+	 * Vérifie si le texte est un palindrome
+	 * @param v un String
+	 * @return
+	 */
+	public static boolean estPalindrome(String v) {
+		boolean pal = true;
+		int i = 0;
+		int j = v.length() - 1;
+		while (pal && i < j) {
+			pal = v.charAt(i) == v.charAt(j);
+			i++;
+			j--;
+		}
+		return pal;
 	}
 
 	public static void main(String[] args) {
-		char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-		afficheVect(hex);
+		/*	char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+			afficheVect(hex);
+		
+			char[] v2 = {};
+			afficheVect(v2);
+		
+			int[] v3 = { 9, 7, 0, 3 };
+			afficheVect(v3);
+			System.out.println("Somme V: " + sommeV(v3));
+			*/
+		// int[] v1 = { 1, 2, 3, 3, 4 };
+		// System.out.println(avgElemV(v1));
+		// char[] vc = { 'A', 'B', 'B', 'A', 'I' };
+		// System.out.println(estPalindrome(vc));
+		// RMinMax r = new RMinMax(5, 76);
+		// System.out.println("Le minimum est: " + r.min() + " Le maximum est: " +
+		// r.max());
+		System.out.println(estPalindrome("RADAR"));
 
-		char[] v2 = {};
-		afficheVect(v2);
-
-		int[] v3 = { 9, 7, 0, 3 };
-		afficheVect(v3);
-		System.out.println("Somme V: " + sommeV(v3));
 	}
 }
--- PAP2425/test/tp02/TestMyVect.java
+++ PAP2425/test/tp02/TestMyVect.java
@@ -1,5 +1,7 @@
 package tp02;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,6 +13,13 @@ class TestMyVect {
 	int[] v2 = { 1, 8, 19 };
 	int[] v3 = { Integer.MAX_VALUE, Integer.MAX_VALUE };
 	int[] v4 = { 19, 2, 3, 8 };
+	char[] txt1 = { 'a' };
+	char[] txt2 = { 'a', 'a' };
+	char[] txt3 = { 'a', 'b', 'b', 'a' };
+	char[] txt4 = { 'r', 'a', 'd', 'a', 'r' };
+	char[] txt5 = { 'z', 'a', 'd', 'a', 'r' };
+	char[] txt6 = { 'r', 'a', 'd', 'o', 'r' };
+	char[] txt7 = { 'r', 'a', 'o', 'r' };
 
 	@Test
 	void testSommeV() {
@@ -25,6 +34,17 @@ class TestMyVect {
 		assertEquals(20, MyVect.maxElemV(v1));
 		assertEquals(19, MyVect.maxElemV(v2));
 		assertEquals(19, MyVect.maxElemV(v4));
+	}
+
+	@Test
+	void testEstPalindrome() {
+		assertTrue(MyVect.estPalindrome(txt1));
+		assertTrue(MyVect.estPalindrome(txt2));
+		assertTrue(MyVect.estPalindrome(txt3));
+		assertTrue(MyVect.estPalindrome(txt4));
+		assertFalse(MyVect.estPalindrome(txt5));
+		assertFalse(MyVect.estPalindrome(txt6));
+		assertFalse(MyVect.estPalindrome(txt7));
 	}
 
 	@Test
