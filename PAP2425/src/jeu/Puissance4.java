95798e1f0dc09b4a9b09b8e0c6ac7619da4a69bf
VOZ <voisfce@gmail.com>
2025-02-20 15:19:40 +0100

    Version Puissance4 pour le prochain TP

--- PAP2425/src/jeu/Puissance4.java
+++ PAP2425/src/jeu/Puissance4.java
@@ -1,5 +1,8 @@
 package jeu;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import tp03.MyMat;
 
 /**
@@ -24,9 +27,15 @@ public class Puissance4 {
 	// Pions
 	private static final int NB_CASES = 6 * 7;
 	private int nbPions;
+	// Position du dernier pion
+	private int lignePion;
+	private int colonnePion;
 
 	// le plateau de jeu avec une bordure (-1)
 	private int[][] jeu = new int[8][9];
+
+	// Scanner pour les entrées de l'utilisateur
+	private Scanner scan;
 
 	/**
 	 * Création d'une nouvelle partie	
@@ -37,6 +46,7 @@ public class Puissance4 {
 		this.joueurJaune = joueur1;
 		this.joueurRouge = joueur2;
 		initPartie();
+		scan = new Scanner(System.in);
 	}
 
 	/**
@@ -44,10 +54,11 @@ public class Puissance4 {
 	 */
 	private void initPartie() {
 		tour = Math.random() < 0.5 ? TOUR_DES_JAUNES : TOUR_DES_ROUGES;
-		initJeu();//initialise la matrice
+		initJeu();// initialise la matrice
 		fin = false;
 		gagne = false;
 		nbPions = 0;
+
 	}
 
 	// actuellement public, utilise ma méthode pour afficher la matrice
@@ -62,18 +73,34 @@ public class Puissance4 {
 	 *  temporairement avec une visibilité "public"
 	 */
 	private void initJeu() {
-
+		for (int i = 0; i < 8; i++)
+			for (int j = 0; j < 9; j++)
+				if (i == 0 || i == 7 || j == 0 || j == 8)
+					jeu[i][j] = BORDURE;
+				else
+					jeu[i][j] = VIDE;
 	}
 
 	/**
+	 * Doit insérer le pion dans le jeu et
+	 * mettre à jour la position du pion inséré (lignePion,colonnePion)
 	 * @param couleur  1 jaune et 2 rouge
 	 * @param colonne  1..7
 	 * @return true si le coup est valable
 	 */
-	public boolean joue(int couleur, int colonne) {
-		boolean ok = true;
-		// TODO
-		return ok;
+	public boolean insertPion(int couleur, int colonne) {
+		if (jeu[1][colonne] != VIDE)
+			return false;
+		int i = 2;
+		// cherche une case occupée
+		while (jeu[i][colonne] == VIDE)
+			i++;
+		i--; // case d'insertion
+		jeu[i][colonne] = couleur;
+		// maj de la position du pion dans le jeu
+		this.lignePion = i;
+		this.colonnePion = colonne;
+		return true;
 	}
 
 	/**
@@ -83,27 +110,85 @@ public class Puissance4 {
 		if (fin) {
 			System.out.println("Lancement d'une nouvelle partie!");
 			initPartie();
-			int col = choisirColonne();
-			
 		}
-		
+		boolean ok;
 		while (!fin) {
 			afficheJeu();
-			
-
+			do {
+				// choix d'une colonne par un joueur (1..7)
+				int col = choisirColonne();
+				ok = insertPion(pionJoueur(), col);
+				if (!ok)
+					System.out.println("La colonne est déjà pleine!!");
+			} while (!ok);
+			nbPions++;// un pion en plus dans le jeu
+			gagne = quattreALaSuite();
+			fin = gagne || nbPions == NB_CASES;
+			if (!fin)// changement joueur
+				tour = !tour;
 		}
 
+		if (gagne)
+			System.out.println("Bravo " + nomJoueur() + " vous avez gagné!!");
+		else
+			System.out.println("Pas de gagnant");
 	}
 
-	private int choisirColonne() {
+	/**
+	 * Retourne true si au moins 4 pions à la suite pour
+	 * le joueur actuel (tour)
+	 * @return true si gagné
+	 */
+	private boolean quattreALaSuite() {
 		// TODO Auto-generated method stub
-		return 0;
+		return false;
+	}
+
+	/**
+	 * Retourne le code du pion du joueur dont c'est le tour
+	 * @return
+	 */
+	private int pionJoueur() {
+		return tour == TOUR_DES_JAUNES ? JAUNE : ROUGE;
+	}
+
+	/**
+	 * Retourne le nom du joueur dont c'est le tour
+	 * @return
+	 */
+	private String nomJoueur() {
+		return tour == TOUR_DES_JAUNES ? joueurJaune : joueurRouge;
+	}
+
+	/**
+	 * Retourne un choix de colonne entre 1 et 7
+	 * @return 1..7
+	 */
+	private int choisirColonne() {
+		int colonne = 0;
+		boolean bad = false;
+		System.out.print(nomJoueur() + ", Entrez un numéro de colonne 1..7: ");
+		do {
+			try {
+				colonne = scan.nextInt();
+				bad = colonne < 1 || colonne > 7;
+			} catch (InputMismatchException e) {
+				bad = true;
+				scan.next();// vide le mauvais caractère du buffer
+			}
+			System.out.println();
+			if (bad)
+				System.out.print("ERREUR, Entrez un nombre entre 1 et 7: ");
+		} while (bad);
+
+		return colonne;
 	}
 
 	public static void main(String[] args) {
 		Puissance4 partie = new Puissance4("Waf", "Vo");
 		partie.start();
-		partie.start();
+
+		// partie.start();
 
 	}
 
